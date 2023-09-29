76d40751b031f53d6a29b396a5bbdd4bb63aedf5
alnabid <anddowzs@gmail.com>
2023-09-30 00:24:05 +0600

    Commit

--- template1/public/js/main.js
+++ template1/public/js/main.js
@@ -1,3 +1,10 @@
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+loader.style.display = "none";
+})
+
+
 $('.a-nav-toggle, .menu-main a').on('click', function(){
     if ($('html').hasClass('body-menu-opened')) {
         $('html').removeClass('body-menu-opened').addClass('body-menu-close');
